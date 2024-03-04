5e3b0a34a6c0bf9029c276f4dada7f2b377f26a9
Ashish-Abhiwan <ashish.abhiwan@gmail.com>
2024-03-04 16:17:20 +0530

    12th commit: change http method to PUT & Delete

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -11,7 +11,14 @@
 
 const categoryModel = require("../models/category.model.js");
 
-// create category
+/**
+ * Create category
+ *
+ * 1. Create a new object from req.body
+ * 2. Check the given category name is already present or not
+ * 3. Create a new category
+ * 4. return the status
+ */
 exports.createCategory = async (req, res) => {
   try {
     // 1. Read the request body and create a category object
@@ -50,13 +57,23 @@ exports.createCategory = async (req, res) => {
   }
 };
 
-// get all categories
-exports.getCategory = async (req, res) => {
+/**
+ * get all categories
+ *
+ * 1. Find all the categories
+ * 2. return the status
+ */
+exports.getCategories = async (req, res) => {
   try {
     const categories = await categoryModel.find({});
     const arrayOfCategory = categories.map((category) => {
       return category.name;
     });
+    if (!arrayOfCategory.length)
+      return res.status(200).send({
+        success: true,
+        message: "Category not available",
+      });
     res.status(200).send({
       success: true,
       message: arrayOfCategory,
@@ -70,10 +87,17 @@ exports.getCategory = async (req, res) => {
   }
 };
 
-// get category by name
+/**
+ * get_category_by_name
+ *
+ * 1. find the category by category.name
+ * 2. Check the category is present or not
+ * 3. return the status
+ */
 exports.getCategoryByName = async (req, res) => {
   try {
     const category = await categoryModel.findOne({ name: req.params.name });
+    console.log(category);
     if (!category) {
       return res.status(404).send({
         success: false,
@@ -93,7 +117,14 @@ exports.getCategoryByName = async (req, res) => {
   }
 };
 
-// update category
+/**
+ * Update category
+ *
+ * 1. find the category by category._id
+ * 2. Check the update name of category already present or not
+ * 3. update the category with new values
+ * 4. return the status
+ */
 exports.updateCategory = async (req, res) => {
   try {
     const searchId = req.params.id;
@@ -106,7 +137,6 @@ exports.updateCategory = async (req, res) => {
     // check, is updated category already present or not ?
     let categoryObj = await categoryModel.findOne({ name: updateObj.name });
     categoryObj = JSON.parse(JSON.stringify(categoryObj));
-
     if (categoryObj && categoryObj._id !== searchId) {
       return res.status(400).send({
         success: false,
@@ -136,6 +166,40 @@ exports.updateCategory = async (req, res) => {
     res.status(500).send({
       success: false,
       message: "Failed ! Error while updating category",
+    });
+  }
+};
+
+/**
+ * Delete category
+ *
+ * 1. find the category by category._id
+ * 2. Check the category is present or not
+ * 3. Delete the category
+ * 4. return the status
+ */
+exports.deleteCategory = async (req, res) => {
+  try {
+    const searchId = req.params.id;
+    const categoryObj = await categoryModel.findOneAndDelete({ _id: searchId });
+    if (!categoryObj)
+      return res.status(404).send({
+        success: false,
+        message: "Failed ! category not found",
+      });
+
+    res.status(200).send({
+      success: true,
+      message: {
+        id: categoryObj._id,
+        name: categoryObj.name,
+      },
+    });
+  } catch (error) {
+    console.log("Error while deleting an category", error);
+    res.status(500).send({
+      success: false,
+      message: "Failed ! Error while deleting an category",
     });
   }
 };
--- routes/category.routes.js
+++ routes/category.routes.js
@@ -6,7 +6,9 @@
  *
  * GET localhost:3005/ecomm/api/v1/get_category_by_name/:name
  *
- * POST localhost:3005/ecomm/api/v1/update-category/{category-id}
+ * PUT localhost:3005/ecomm/api/v1/update_category/:id
+ * 
+ * DELETE localhost:3005/ecomm/api/v1/delete_category/:id
  *
  * I need to intercept this
  *
@@ -31,27 +33,31 @@ module.exports = (app) => {
   app.get(
     "/ecomm/api/v1/get_categories",
     [categoryMiddleware.verifyToken, categoryMiddleware.verifyUserType],
-    categoryController.getCategory
+    categoryController.getCategories
   );
 
   // get single category
   app.get(
     "/ecomm/api/v1/get_category_by_name/:name",
-    [
-      categoryMiddleware.verifyToken,
-      categoryMiddleware.verifyUserType,
-    ],
+    [categoryMiddleware.verifyToken, categoryMiddleware.verifyUserType],
     categoryController.getCategoryByName
   );
 
   // update category
   app.put(
-    "/ecomm/api/v1/update-category/:id",
+    "/ecomm/api/v1/update_category/:id",
     [
       categoryMiddleware.checkDetails,
       categoryMiddleware.verifyToken,
       categoryMiddleware.verifyUserType,
     ],
     categoryController.updateCategory
+  );
+
+  // delete category
+  app.delete(
+    "/ecomm/api/v1/delete_category/:id",
+    [categoryMiddleware.verifyToken, categoryMiddleware.verifyUserType],
+    categoryController.deleteCategory
   );
 };
